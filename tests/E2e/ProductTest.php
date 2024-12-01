abff8525f72092f0c6c8dfc65e9d8ea7a10c263a
Miguel Olmos <mglolmos@Miguels-Laptop.fritz.box>
2024-12-01 22:05:28 +0100

    Added some comments

--- tests/E2e/ProductTest.php
+++ tests/E2e/ProductTest.php
@@ -19,29 +19,36 @@ class ProductTest extends TestCase
 
     public function testCreateGetAndDeleteProduct(): void
     {
+        // Arrange: Create product
         $product_id = Uuid::generateToString();
         $product_name = 'Product Test' . $product_id;
+        // Act
         $response = $this->client->post('http://nginx/product', [
             RequestOptions::FORM_PARAMS => [
                 'product_id' => $product_id,
                 'name' => $product_name
             ],
         ]);
-
+        // Assert
         $this->assertEquals(201, $response->getStatusCode());
 
+        // Act: Get product
         $response = $this->client->get('http://nginx/product/' . $product_id);
-
+        // Assert
         $this->assertEquals(200, $response->getStatusCode());
         $this->assertEquals($product_id, self::getOutputFromResponse($response, 'product_id'));
         $this->assertEquals($product_name, self::getOutputFromResponse($response, 'name'));
 
+        // Act: Delete product
         $response = $this->client->delete('http://nginx/product/' . $product_id);
+        // Assert
         $this->assertEquals(204, $response->getStatusCode());
 
+        // Act: Get product, expected 404
         try {
              $this->client->get('http://nginx/product/' . $product_id);
         } catch (RequestException $exception) {
+            // Assert
             $this->assertEquals(404, $exception->getResponse()->getStatusCode());
             return;
         }
@@ -51,9 +58,12 @@ class ProductTest extends TestCase
     public function testProductNotFoundShouldAnswer404(): void
     {
         try {
+            // Arrange
             $product_id = Uuid::generateToString();
+            // Act: Get product, expected 404
             $this->client->get('http://nginx/product/' . $product_id);
         } catch (RequestException $exception) {
+            // Assert
             $this->assertEquals(404, $exception->getResponse()->getStatusCode());
             return;
         }
@@ -63,48 +73,59 @@ class ProductTest extends TestCase
 
     public function testAddAndGetCollection(): void
     {
+        // Arrange
         $product_id = $this->createProduct();
         $collection_name = 'Collection name';
 
+        // Act: Create collection
         $response = $this->client->post('http://nginx/product/' . $product_id . '/collection', [
             RequestOptions::FORM_PARAMS => [
                 'name' => $collection_name
             ],
         ]);
+        // Assert
         $this->assertEquals(201, $response->getStatusCode());
         $this->assertEquals($product_id, self::getOutputFromResponse($response, 'product_id'));
         $this->assertEquals('collection_name', self::getOutputFromResponse($response, 'collection_id') );
         $this->assertEquals($collection_name, self::getOutputFromResponse($response, 'collection_name') );
 
+        // Arrange
         $collection_id = self::getOutputFromResponse($response, 'collection_id');
 
+        // Act: Get collection
         $response = $this->client->get('http://nginx/product/' . $product_id . '/collection/' . $collection_id);
+        // Assert
         $this->assertEquals($collection_id, self::getOutputFromResponse($response, 'collection_id') );
         $this->assertEquals($collection_name, self::getOutputFromResponse($response, 'collection_name') );
     }
 
     public function testGetAndGetComponent(): void
     {
+        // Arrange
         $product_id = $this->createProduct();
         $collection_id = $this->addCollection($product_id, 'Collection Test');
         $component_name = 'Component name';
         $component_price = 100;
 
+        // Act: Get component
         $response = $this->client->post('http://nginx/product/' . $product_id . '/collection/' . $collection_id . '/component', [
             RequestOptions::FORM_PARAMS => [
                 'name' => $component_name,
                 'price' => $component_price
             ],
         ]);
-
+        // Assert
         $this->assertEquals(201, $response->getStatusCode());
         $this->assertEquals($product_id, self::getOutputFromResponse($response, 'product_id'));
         $this->assertEquals('component_name', self::getOutputFromResponse($response, 'component_id') );
         $this->assertEquals($component_name, self::getOutputFromResponse($response, 'component_name') );
 
+        // Arrange
         $component_id = self::getOutputFromResponse($response, 'component_id');
 
+        // Act: Get component
         $response = $this->client->get('http://nginx/product/' . $product_id . '/collection/' . $collection_id . '/component/' . $component_id);
+        // Assert
         $this->assertEquals($component_id, self::getOutputFromResponse($response, 'component_id') );
         $this->assertEquals($component_name, self::getOutputFromResponse($response, 'component_name') );
         $this->assertEquals($component_price, self::getOutputFromResponse($response, 'component_price') );
